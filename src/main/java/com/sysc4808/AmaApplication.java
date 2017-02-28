58886f8af6eef903bec09ff7d99b5b00a462d54e
Richard Carson <rscarson@rogers.com>
2017-02-28 17:20:42 -0500

    Add base project

---
+++ src/main/java/com/sysc4808/AmaApplication.java
@@ -0,0 +1,12 @@
+package com.sysc4808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaApplication.class, args);
+	}
+}

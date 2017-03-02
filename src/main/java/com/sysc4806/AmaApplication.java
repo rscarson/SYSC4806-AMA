484e4b8ebff3e06bd3e297f2a3adee5624e696c4
max.demelo2267@gmail.com <max.demelo2267@gmail.com>
2017-03-02 09:28:37 -0500

    Fixed package names

--- src/main/java/com/sysc4806/AmaApplication.java
+++ src/main/java/com/sysc4806/AmaApplication.java
@@ -1,4 +1,4 @@
-package com.sysc4808;
+package com.sysc4806;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sysc4806/AmaApplicationTests.java
+++ src/test/java/com/sysc4806/AmaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sysc4808;
+package com.sysc4806;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;

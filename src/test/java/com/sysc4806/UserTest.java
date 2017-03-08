4222494994818ef38f397848d36e852f9b9f11cc
Max DeMelo <max.demelo2267@gmail.com>
2017-03-08 10:36:22 -0500

    UserController all hooked up and ready to go

--- src/test/java/com/sysc4806/UserTest.java
+++ src/test/java/com/sysc4806/UserTest.java
@@ -1,7 +1,35 @@
 package com.sysc4806;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * Created by maxwelldemelo on 3/2/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class UserTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnUserIndex() throws Exception {
+        this.mockMvc.perform(get("/user")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnNewUserFrom() throws Exception {
+        this.mockMvc.perform(get("/user/new")).andDo(print()).andExpect(status().isOk());
+    }
 }

9f5e445de0674957e94a2e97b043b499cbe08cab
Adam J Batson <adamjbatson@gmail.com>
2017-03-16 10:29:01 -0400

    ENhance PostContollerTests fix #35

--- src/test/java/com/sysc4806/PostControllerTest.java
+++ src/test/java/com/sysc4806/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.sysc4806;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +26,9 @@ public class PostControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private PostRepository postRepo;
+
 
     @Test
     public void shouldReturnAMAIndex() throws Exception {
@@ -33,5 +38,29 @@ public class PostControllerTest {
     @Test
     public void shouldReturnNewAMAFrom() throws Exception {
         mockMvc.perform(get("/ama/new")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCreateANewPost() throws Exception {
+        mockMvc.perform(post("/ama/new")
+                    .param("username", "adambatson")
+                    .param("title", "new AMA")
+                    .param("description", "Ask me Anything!")
+                    .param("tags", "AMA, X, Y, adam"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+    }
+
+    @Test
+    public void shouldRenderAnAMAView() throws Exception {
+        shouldCreateANewPost();
+        mockMvc.perform(get("/ama/view?id=1")).andExpect(status().isOk());
+}
+
+    @Test
+    public void should404OnMissingAMA() throws Exception {
+        mockMvc.perform((get("/ama/view?id=66")))
+                .andExpect(status().is4xxClientError());
     }
 }

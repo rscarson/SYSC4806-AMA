f72596ab0b950455c01616e012c1e8738793f4fb
Richard Carson <rscarson@rogers.com>
2017-03-23 09:52:17 -0400

    Add basic OAUTH2 config

--- src/main/java/com/sysc4806/AmaApplication.java
+++ src/main/java/com/sysc4806/AmaApplication.java
@@ -2,8 +2,10 @@ package com.sysc4806;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class AmaApplication {
 
 	public static void main(String[] args) {

b758217ef6cddefe6a1f95efe605d8ef3db7963a
Adam J Batson <adamjbatson@gmail.com>
2017-04-04 10:14:23 -0400

    Now able to sort on user overview page, user overview page complete.  Fix #74

--- src/main/java/com/sysc4806/CommentRepository.java
+++ src/main/java/com/sysc4806/CommentRepository.java
@@ -1,12 +1,14 @@
 package com.sysc4806;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by Richard on 3/21/2017.
  */
+@Repository
 public interface CommentRepository  extends CrudRepository<Comment, Long> {
     public List<Comment> findByPostAndParent(Post post, Comment parent);
 
@@ -19,5 +21,13 @@ public interface CommentRepository  extends CrudRepository<Comment, Long> {
     public List<Comment> findByPostAndParentOrderByCreatedAsc(Post post, Comment parent);
 
     public List<Comment> findByPoster(User poster);
+
+    public List<Comment> findByPosterOrderByVotesAsc(User poster);
+
+    public List<Comment> findByPosterOrderByVotesDesc(User poster);
+
+    public List<Comment> findByPosterOrderByCreatedDesc(User poster);
+
+    public List<Comment> findByPosterOrderByCreatedAsc(User poster);
 
 }
